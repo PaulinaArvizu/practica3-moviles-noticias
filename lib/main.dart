2c7c061b9d581f16b2532b512d0e356381bcbbda
PaulinaArvizu <is707571@iteso.mx>
2020-11-13 21:30:08 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:noticias/home_page.dart';
+import 'package:noticias/login/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
