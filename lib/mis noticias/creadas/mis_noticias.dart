fec2f8c52fa8e3208791714a7371eb3507b2c904
PaulinaArvizu <is707571@iteso.mx>
2020-11-13 21:35:52 -0600

    Poner refreshIndicator para actualizar lista; agregar snackbar de error

--- lib/mis noticias/creadas/mis_noticias.dart
+++ lib/mis noticias/creadas/mis_noticias.dart
@@ -4,27 +4,40 @@ import 'package:noticias/mis%20noticias/bloc/mis_noticias_bloc.dart';
 import 'package:noticias/noticias/item_noticia.dart';
 
 class MisNoticias extends StatelessWidget {
-  const MisNoticias({Key key}) : super(key: key);
+  MisNoticias({Key key}) : super(key: key);
   //DONE: mostrar noticias guradadas en firebase
+  MisNoticiasBloc _bloc = MisNoticiasBloc();
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Mis noticias'),
       ),
       body: BlocProvider(
-        create: (context) => MisNoticiasBloc()..add(LeerNoticiasEvent()),
+        create: (context) {
+          return _bloc..add(LeerNoticiasEvent());
+        },
         child: BlocConsumer<MisNoticiasBloc, MisNoticiasState>(
           listener: (context, state) {
-            //
+            //DONE: dialogo o snackbar de error
+            _scaffoldKey.currentState.showSnackBar(
+                SnackBar(content: Text("Error al solicitar noticias")));
           },
           builder: (context, state) {
             if (state is NoticiasDescargadasState) {
               return Scaffold(
-                body: ListView.builder(
-                  itemCount: state.noticiasList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ItemNoticia(noticia: state.noticiasList[index]);
+                body: RefreshIndicator(
+                  child: ListView.builder(
+                    itemCount: state.noticiasList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ItemNoticia(noticia: state.noticiasList[index]);
+                    },
+                  ),
+                  onRefresh: () async {
+                    _bloc..add(LeerNoticiasEvent());
+                    return Future.delayed(Duration(seconds: 1));
                   },
                 ),
               );
